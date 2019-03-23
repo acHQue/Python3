09fe10212c2fb5aa304c40f75838ee756223399f
acHQue <32178723+acHQue@users.noreply.github.com>
2019-03-23 12:57:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+#  The Web Scraper Project
 #
-#   The Web Scraper Project
-#
-
+import urllib.request
 import datetime
 
-x = datetime.datetime.now()
-#y = x.year
+x = -1
+startYear = 1994
+year = datetime.datetime.now()
+y = (int(year.strftime("%Y")))
+rng = y - startYear
+print("The National Lottery started in", startYear)
+print("Loop starts at",startYear)
 
-#xml = urlopen("https://www.lottery.co.uk/lotto/results/past").read()
+for i in range(startYear, y):           # So i is the itorates the range, and since the range is specified to be 1994, hardcoded, the sytem time specifies the stop date y.
+                                        #We get output 1994 (start of range to 20**).
 
-import urllib.request
+    #downloading data
+    print(i)
+    stringYear = str(i)
+    u = urllib.request.urlretrieve("https://www.lottery.co.uk/lotto/results/archive-" + stringYear, stringYear + ".txt")
 
-#urllib.request.urlretrieve("https://www.lottery.co.uk/lotto/results/past", "test.txt")
 
-year = x.year
-#int(year -1)
-#print(int(year - 1 ))
 
-for year in range(1994, year):
-    #print('https://www.lottery.co.uk/lotto/results/archive-' + str(year -1 ))
-    #if year == 1994: continue
 
-    print(year)
-
-#1994
 
 #urllib.request.url
+# year = x.year
+# print('https://www.lottery.co.uk/lotto/results/archive-' + str(year -1 ))
+# if year == 1994: continue
 
+
+
+# 1994
 
 
